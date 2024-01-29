79b317db6b8f018079b352b284b1b79fa37d85a2
wisnu31899 <92870248+wisnu31899@users.noreply.github.com>
2024-01-29 10:07:29 +0700

    new upd task2 go1

--- generatePassword.go
+++ generatePassword.go
@@ -4,63 +4,123 @@ import (
 	"math/rand"
 )
 
-func generatePassword(password, level string) string {
+// func generatePassword(password, level string) string {
 
+// 	// Memastikan panjang password minimal 6 karakter
+// 	if len(password) < 6 {
+// 		return "Password length should be at least 6 characters."
+// 	}
+
+// 	// Fungsi untuk mendapatkan karakter acak dari setiap jenis karakter
+// 	getRandomLower := func() byte {
+// 		randomLower := "abcdefghijklmnopqrstuvwxyz"
+// 		return randomLower[rand.Intn(len(randomLower))]
+// 	}
+// 	getRandomUpper := func() byte {
+// 		RandomUpper := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// 		return RandomUpper[rand.Intn(len(RandomUpper))]
+// 	}
+// 	getRandomDigit := func() byte {
+// 		RandomDigit := "0123456789"
+// 		return RandomDigit[rand.Intn(len(RandomDigit))]
+// 	}
+// 	getRandomSpecial := func() byte {
+// 		specialChars := "!@#$%^&*()-_=+[]{}|;:'\",.<>/?"
+// 		return specialChars[rand.Intn(len(specialChars))]
+// 	}
+
+// 	var newPassword []byte //buatkan var dgn type slice byte
+
+// 	for i := 0; i < len(password); i++ {
+// 		if level == "low" {
+// 			randType := rand.Intn(2)
+// 			if randType == 0 {
+// 				newPassword = append(newPassword, getRandomLower())
+// 			} else if randType == 1 {
+// 				newPassword = append(newPassword, getRandomUpper())
+// 			}
+// 		} else if level == "medium" {
+// 			randType := rand.Intn(3)
+// 			if randType == 0 {
+// 				newPassword = append(newPassword, getRandomLower())
+// 			} else if randType == 1 {
+// 				newPassword = append(newPassword, getRandomUpper())
+// 			} else if randType == 2 {
+// 				newPassword = append(newPassword, getRandomDigit())
+// 			}
+// 		} else if level == "strong" {
+// 			randType := rand.Intn(4)
+// 			if randType == 0 {
+// 				newPassword = append(newPassword, getRandomLower())
+// 			} else if randType == 1 {
+// 				newPassword = append(newPassword, getRandomUpper())
+// 			} else if randType == 2 {
+// 				newPassword = append(newPassword, getRandomDigit())
+// 			} else if randType == 3 {
+// 				newPassword = append(newPassword, getRandomSpecial())
+// 			}
+// 		}
+// 	}
+
+// 	return string(newPassword)
+// }
+
+func generatePassword(password, level string) string {
 	// Memastikan panjang password minimal 6 karakter
 	if len(password) < 6 {
 		return "Password length should be at least 6 characters."
 	}
 
 	// Fungsi untuk mendapatkan karakter acak dari setiap jenis karakter
-	getRandomLower := func() byte {
+	getRandomLower := func() string {
 		randomLower := "abcdefghijklmnopqrstuvwxyz"
-		return randomLower[rand.Intn(len(randomLower))]
+		return string(randomLower[rand.Intn(len(randomLower))])
 	}
-	getRandomUpper := func() byte {
+	getRandomUpper := func() string {
 		RandomUpper := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		return RandomUpper[rand.Intn(len(RandomUpper))]
+		return string(RandomUpper[rand.Intn(len(RandomUpper))])
 	}
-	getRandomDigit := func() byte {
+	getRandomDigit := func() string {
 		RandomDigit := "0123456789"
-		return RandomDigit[rand.Intn(len(RandomDigit))]
+		return string(RandomDigit[rand.Intn(len(RandomDigit))])
 	}
-	getRandomSpecial := func() byte {
+	getRandomSpecial := func() string {
 		specialChars := "!@#$%^&*()-_=+[]{}|;:'\",.<>/?"
-		return specialChars[rand.Intn(len(specialChars))]
+		return string(specialChars[rand.Intn(len(specialChars))])
 	}
 
-	var newPassword []byte //buatkan var dgn type slice byte
+	var newPassword string // ubah tipe data ke string
 
 	for i := 0; i < len(password); i++ {
 		if level == "low" {
 			randType := rand.Intn(2)
 			if randType == 0 {
-				newPassword = append(newPassword, getRandomLower())
+				newPassword += getRandomLower()
 			} else if randType == 1 {
-				newPassword = append(newPassword, getRandomUpper())
+				newPassword += getRandomUpper()
 			}
 		} else if level == "medium" {
 			randType := rand.Intn(3)
 			if randType == 0 {
-				newPassword = append(newPassword, getRandomLower())
+				newPassword += getRandomLower()
 			} else if randType == 1 {
-				newPassword = append(newPassword, getRandomUpper())
+				newPassword += getRandomUpper()
 			} else if randType == 2 {
-				newPassword = append(newPassword, getRandomDigit())
+				newPassword += getRandomDigit()
 			}
 		} else if level == "strong" {
 			randType := rand.Intn(4)
 			if randType == 0 {
-				newPassword = append(newPassword, getRandomLower())
+				newPassword += getRandomLower()
 			} else if randType == 1 {
-				newPassword = append(newPassword, getRandomUpper())
+				newPassword += getRandomUpper()
 			} else if randType == 2 {
-				newPassword = append(newPassword, getRandomDigit())
+				newPassword += getRandomDigit()
 			} else if randType == 3 {
-				newPassword = append(newPassword, getRandomSpecial())
+				newPassword += getRandomSpecial()
 			}
 		}
 	}
 
-	return string(newPassword)
+	return newPassword
 }
